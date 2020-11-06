8f5969ae7369e1870535817638c4676975f6c299
Romavic dos Anjos <romavicdosanjoskc@gmail.com>
2020-11-06 14:59:27 +0100

    Ui and Time task for to delayed SplashScreen.

---
+++ app/src/main/java/ao/inokri/devfestangola/ui/pages/SplashActivity.kt
@@ -0,0 +1,41 @@
+package ao.inokri.devfestangola.ui.pages
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import ao.inokri.devfestangola.R
+import ao.inokri.devfestangola.data.viewmodels.SplashViewModel
+import ao.inokri.devfestangola.data.viewmodelsfactory.ViewModelFactory
+import ao.inokri.devfestangola.ui.pages.main.MainActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashViewModel: SplashViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        splashViewModel = ViewModelProvider(
+            this, ViewModelFactory()
+        )[SplashViewModel::class.java]
+    }
+
+    override fun onStart() {
+        timeSplash()
+        super.onStart()
+    }
+
+    private fun timeSplash() {
+        GlobalScope.launch {
+            delay(3500).run {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
